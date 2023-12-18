93cd7b7c3a4037fc6434f88ab9afc14b0b3fc633
영현 <132960024+kyh234579@users.noreply.github.com>
2023-12-18 16:19:49 +0900

    Update 1218_백준9465_스티커.py

---
+++ 1218_백준9465_스티커.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+T=int(input())
+
+for _ in range(T):
+    arr=[]
+    n=int(input())
+    for _ in range(2):
+       arr.append(list(map(int,input().split())))
+    DP=[[0]*n for _ in range(2)]
+
+    DP[0][0] = arr[0][0]
+    DP[1][0] = arr[1][0]
+    if n==1:
+        print(max(DP[0][0],DP[1][0]))
+        continue
+    DP[0][1] = arr[1][0]+arr[0][1]
+    DP[1][1] = arr[0][0]+arr[1][1]
+    if n==2:
+        print(max(DP[0][1],DP[1][1]))
+        continue
+
+    for i in range(2,n):
+        DP[0][i] = max(DP[1][i-2],DP[1][i-1]) + arr[0][i]
+        DP[1][i] = max(DP[0][i-2],DP[0][i-1]) + arr[1][i]
+
+    print(max(DP[0][-1],DP[1][-1]))
