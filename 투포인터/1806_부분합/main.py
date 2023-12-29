42ea810915a64fd32ea6de2e8031b9935687409b
kyh234579 <kyh234579@gmail.com>
2023-12-29 16:13:31 +0900

    [투포인터_부분합 풀이]

---
+++ 투포인터/1806_부분합/main.py
@@ -0,0 +1,22 @@
+n,s=map(int,input().split())
+arr=list(map(int,input().split()))
+sum=arr[0]
+min_length=float("inf")
+left,right=0,0
+
+while left <= right:
+    if sum >=s:
+        min_length=min(min_length,right-left+1)
+        sum-=arr[left]
+        left+=1
+    else:
+        right+=1
+        if right < n:
+            sum+=arr[right]
+        else:
+            break
+        
+if min_length == float("inf"):
+    print(0)
+else:
+    print(min_length)
