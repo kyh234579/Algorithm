1d4d656646a9a8b7af4f3517495043a90ad0671a
kyh234579 <kyh234579@gmail.com>
2024-02-23 18:24:05 +0900

    백트래킹,본인 포함/미포함 구분 재귀

---
+++ 완전탐색/2961_도영이가 만든 맛있는 음식/main.py
@@ -0,0 +1,22 @@
+n=int(input())
+ingredients=[]
+for _ in range(n):
+    s,b=map(int,input().split())
+    ingredients.append([s,b])
+
+min_value=1e9
+def dfs(idx,a,b,cnt):
+    global min_value
+    #가지치기
+    if cnt>0:
+        min_value=min(min_value,abs(a-b))
+    #깊이
+    if idx==n:
+        return
+    #본인포함
+    dfs(idx+1,a*ingredients[idx][0],b+ingredients[idx][1],cnt+1)
+    #미포함,인덱스만 증가시켜 다음재료로 넘어감.
+    dfs(idx+1,a,b,cnt)
+
+dfs(0,1,0,0)
+print(min_value)
